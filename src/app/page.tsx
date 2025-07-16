bfaffb4d93053c2135f2f29f0ae02def943d203e
Prakash Pandey <prakash.pandey@your-space.in>
2025-07-16 23:44:16 +0530

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
-import {prisma} from "@/lib/db";
+import { caller } from "@/trpc/server";
 
 export default async function Home() {
+  // You can use the `caller` to call your TRPC procedures directly
+  const {greeting} = await caller.createAi({
+    text: "Prakash",})
   return (
     <>
       <Button variant={"outline"} > Hii There </Button>
+      <p >{greeting}</p>
     </>
   );
 }
