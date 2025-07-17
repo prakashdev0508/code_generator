5562506e22b521daaf78513e1a510b478c6b0dfb
Prakash Pandey <prakash.pandey@your-space.in>
2025-07-18 01:43:29 +0530

    ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -20,9 +25,15 @@ export default function Home() {
 
   return (
     <div className="p-4">
+      <Textarea
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Enter Some thing "
+        className="mb-5 "
+      />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ email: "prakash@gmail.com" })}
+        onClick={() => invoke.mutate({ email: value })}
       >
         {" "}
         {invoke.isPending
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,23 @@
+import { gemini, openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
+    const writer = createAgent({
+      name: "Code writer ",
+      system:
+        "You are an expert nextjs and reactjs code snippet writer. Write code that is clean, efficient, and follows best practices and with good ui using tailwind css classes if reqiured of styling. Always give only one code snippet in response which is best with no explanation just code snippet .",
+      model: gemini({ model: "gemini-1.5-flash" }),
+    });
 
-    await step.sleep("wait-a-moment 2 ", "10s");
+    // Run the agent with an input.  This automatically uses steps
+    // to call your AI model.
+    const { output } = await writer.run(
+      `Wirte the code for: ${event.data.email}`
+    );
 
-    return { message: `Hello ! ${event.data.email}` };
+    return { message: output[0] };
   }
 );
