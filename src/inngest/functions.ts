08aa295ff3b15177ab244a8fb49208f264c0ae75
Prakash Pandey <prakash.pandey@your-space.in>
2025-07-18 03:14:34 +0530

    e2b sandbox added

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { gemini, openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandBox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("code-gen-nextjs-2");
+      return sandbox.sandboxId;
+    });
+
     const writer = createAgent({
       name: "Code writer ",
       system:
@@ -18,6 +25,15 @@ export const helloWorld = inngest.createFunction(
       `Wirte the code for: ${event.data.email}`
     );
 
-    return { message: output[0] };
+    const sandBoxURl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandBox(sandboxId);
+      const host = sandbox.getHost(3000);
+
+      return `http://${host}`;
+    });
+
+
+
+    return { message: output[0] , sandboxId, sandBoxURl  };
   }
 );
